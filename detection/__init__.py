53d3db3139f3c116bb656b388c7ef6f8106b43dd
Ali-Nigzu <c.razakali@gmail.com>
2026-07-06 17:13:19 +0100

    feat: implement DetectV2 contract

---
+++ detection/__init__.py
@@ -0,0 +1,17 @@
+"""Detection modules."""
+
+from .detect_v2 import (
+    DetectV2,
+    DetectV2Config,
+    DetectV2InputError,
+    RawDetection,
+    detect_v2,
+)
+
+__all__ = [
+    "DetectV2",
+    "DetectV2Config",
+    "DetectV2InputError",
+    "RawDetection",
+    "detect_v2",
+]
