90868ef2eb2eabaabe8d7952bdccad1a932a0d2b
Acamoseng50 <ali9@cenos.uk>
2026-08-21 15:51:01 +0100

    Extract production Analyze orchestrator

---
+++ analyze/__init__.py
@@ -0,0 +1,3 @@
+from .analyze import Analyze
+
+__all__ = ["Analyze"]
--- test_tracking_v2_pipeline.py
+++ test_tracking_v2_pipeline.py
@@ -4,38 +4,14 @@ from __future__ import annotations
 
 import json
 
-from assemble import Assemble
-from demographics import Demographic
-from detect import Detect
-from events import Event
-from initialise import initialise
-from load.load import load
-from send import Send
-from track import Track
-from update import update
+from analyze import Analyze
 from visualise import Visualise
 
 DEVICE_ID = 1
 
 
 def main() -> None:
-    context = initialise(DEVICE_ID)
-    frame_batch = load(
-        context["gcs_source_uri"],
-        context["timeframe"],
-    )
-    detection_batch = Detect()(frame_batch)
-    track_batch = Track()(detection_batch)
-    event_batch = Event()(track_batch, context["analysis_config"])
-    demographics_batch = Demographic()(event_batch, frame_batch)
-    output_batch = Assemble()(
-        event_batch,
-        demographics_batch,
-        context["timeframe"]["start"],
-        context["device_id"],
-    )
-    Send()(output_batch, context["bigquery_destination"])
-    update(context["device_id"], context["timeframe"]["end"])
+    output_batch = Analyze(DEVICE_ID)
     print(json.dumps(output_batch, sort_keys=True))
 
 
